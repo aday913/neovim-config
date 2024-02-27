ff8286155a55e3a690181a11f25eed49625b62c3
Alex Day <alexdaygjj@gmail.com>
2024-02-27 13:00:24 -0700

    Add quick navigation to harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -5,5 +5,7 @@ return {
 		vim.keymap.set("n", "<leader>hm", harpoon_mark.add_file, {})
     local harpoon_ui = require('harpoon.ui')
     vim.keymap.set("n", "<leader>hu", harpoon_ui.toggle_quick_menu, {})
+    vim.keymap.set("n", "<C-b>", harpoon_ui.nav_prev, {})
+    vim.keymap.set("n", "<C-n>", harpoon_ui.nav_next, {})
 	end,
 }
