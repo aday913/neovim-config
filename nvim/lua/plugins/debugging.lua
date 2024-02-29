9067166c4c766705c32e9640ed76eb1bf61e46c2
Alex Day <alexdaygjj@gmail.com>
2024-02-29 14:32:19 -0700

    Add python debugging

---
+++ nvim/lua/plugins/debugging.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"mfussenegger/nvim-dap-python",
+		ft = "python",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"rcarriga/nvim-dap-ui",
+		},
+		config = function(_, _)
+			local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+			require("dap-python").setup(path)
+			vim.keymap.set("n", "<leader>dp", require("dap-python").test_method, {})
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+			vim.keymap.set("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>")
+		end,
+	},
+}
