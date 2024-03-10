ee7d54643d62c08f17168d83d559a8fa65ba4825
Alex Day <alexdaygjj@gmail.com>
2024-03-09 22:05:56 -0700

    Add obsidian nvim

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,12 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*",
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		dir = "~/main-notes",
+	},
+}
