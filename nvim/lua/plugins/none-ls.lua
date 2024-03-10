b829417c3c1d09daed0e4551796be888c2f18a98
Alex Day <alexdaygjj@gmail.com>
2024-03-09 22:05:07 -0700

    Comment out mypy diagnostics

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,7 +6,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.mypy,
+        -- null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.formatting.goimports_reviser
 			},
 		})
